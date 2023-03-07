9d75b4953453ecad9f99de836eceafde02779111
kemaltf <kemaltaufikf@gmail.com>
2023-03-07 12:41:51 +0800

    type aliases (2) - object type aliases

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,7 +1,8 @@
 type StringOrNum = string | number;
+type objWithName = { name: string; uid: StringOrNum };
 const greet = (user: { name: string; uid: StringOrNum }) => {
   console.log(`${user.name} says hello!`);
 };
-const greetAgain = (user: { name: string; uid: StringOrNum }) => {
+const greetAgain = (user: objWithName) => {
   console.log(`${user.name} says hello!`);
 };
