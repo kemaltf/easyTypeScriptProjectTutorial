ffa82c6fd7826d8656221dd5310376ed19ebfb6e
kemaltf <kemaltaufikf@gmail.com>
2023-03-07 14:28:56 +0800

    "create function signature"

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,8 +1,26 @@
-type StringOrNum = string | number;
-type objWithName = { name: string; uid: StringOrNum };
-const greet = (user: objWithName) => {
-  console.log(`${user.name} says hello!`);
+// example 1: output void
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}!`);
 };
-const greetAgain = (user: objWithName) => {
-  console.log(`${user.name} says hello!`);
+
+// example 2 output number
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === "add") {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+};
+
+// example 3 using object as parameter
+let logDetails: (obj: { name: string; age: number }) => void;
+
+type person = { name: string; age: number };
+
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja.age} y.o`);
 };
