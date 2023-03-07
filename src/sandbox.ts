7d97d9cb373c38e9fd641c1cbffb17e95697cb6c
kemaltf <kemaltaufikf@gmail.com>
2023-03-07 12:23:58 +0800

    function basics (4) - Ts automatically gives return type

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -4,7 +4,16 @@ greet = () => {
   console.log("hello");
 };
 
-const add = (a: number, b: number, c: number) => {
+// if we want to add optional parameter we can use 2 ways
+// first, add '?' before ':' e.g: c?:
+// second, we can add optional paramteter by using default value
+const add = (a: number, b: number, c: number = 10) => {
   console.log(a + b);
 };
 add(5, 10);
+
+const minus = (a: number, b: number) => {
+  return a - b;
+};
+let resMinus = minus(4, 3);
+resMinus = "this gonna be an error";
