121a385ee31e14f331bb40c1b3e5f996bab2e52b
kemaltf <kemaltaufikf@gmail.com>
2023-03-07 12:40:35 +0800

    type aliases (1) - basic type aliases

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,19 +1,7 @@
-let greet: Function;
-
-greet = () => {
-  console.log("hello");
+type StringOrNum = string | number;
+const greet = (user: { name: string; uid: StringOrNum }) => {
+  console.log(`${user.name} says hello!`);
 };
-
-// if we want to add optional parameter we can use 2 ways
-// first, add '?' before ':' e.g: c?:
-// second, we can add optional paramteter by using default value
-const add = (a: number, b: number, c: number = 10): void => {
-  console.log(a + b);
+const greetAgain = (user: { name: string; uid: StringOrNum }) => {
+  console.log(`${user.name} says hello!`);
 };
-add(5, 10);
-
-const minus = (a: number, b: number): number => {
-  return a - b;
-};
-let resMinus = minus(4, 3);
-resMinus = "this gonna be an error";
