18dc35a0658dddc1369716c340ea89ba8cbf4a5f
kemaltf <kemaltaufikf@gmail.com>
2023-03-08 12:41:57 +0800

    create classes basic

--- public/app.js
+++ public/app.js
@@ -1,4 +1,18 @@
 "use strict";
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+console.log(invOne, invTwo);
 // const form = document.querySelector("form");
 const form = document.querySelector(".new-item-form");
 console.log(form.children);
