f65884665b97e4bd3d67a266d548357879b8ebc6
kemaltf <kemaltaufikf@gmail.com>
2023-03-09 10:30:13 +0800

    modules in typescript

--- public/app.js
+++ public/app.js
@@ -1,15 +1,4 @@
-"use strict";
-// classes
-class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/Invoice.js";
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 console.log(invOne, invTwo);
@@ -18,12 +7,13 @@ let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 // readonly cannot re-assign but can read inside or outside the class
-invOne.client = "yoshi";
+// invOne.client = "yoshi";
 invTwo.amount = 2000;
 console.log("changed data:", invOne, invTwo);
 invoices.forEach((inv) => {
     // private only can read inside the class
-    console.log(inv.client, inv.details, inv.amount, inv.format());
+    // console.log(inv.client, inv.details, inv.amount, inv.format());
+    console.log(inv.client, inv.amount, inv.format());
 });
 // const form = document.querySelector("form");
 const form = document.querySelector(".new-item-form");
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,4 @@
-// classes
-class Invoice {
-  readonly client: string;
-  private details: string;
-  public amount: number;
-
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from "./classes/Invoice.js";
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 
@@ -22,13 +8,14 @@ let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 // readonly cannot re-assign but can read inside or outside the class
-invOne.client = "yoshi";
+// invOne.client = "yoshi";
 invTwo.amount = 2000;
 
 console.log("changed data:", invOne, invTwo);
 invoices.forEach((inv) => {
   // private only can read inside the class
-  console.log(inv.client, inv.details, inv.amount, inv.format());
+  // console.log(inv.client, inv.details, inv.amount, inv.format());
+  console.log(inv.client, inv.amount, inv.format());
 });
 
 // const form = document.querySelector("form");
